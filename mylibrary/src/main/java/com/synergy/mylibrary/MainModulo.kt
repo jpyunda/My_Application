8606cf815ed2448ddc7a469ab41289be1888c24f
Juan Pablo <jpyunda@gmail.com>
2024-12-05 11:56:23 -0500

    cambio de color a azul

---
+++ mylibrary/src/main/java/com/synergy/mylibrary/MainModulo.kt
@@ -0,0 +1,35 @@
+package com.synergy.mylibrary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.synergy.mylibrary.ui.theme.MyApplicationTheme
+
+//class MainModulo : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContent {
+//            MyApplicationTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}
+class LibraryHelper {
+    fun greet() = "Hello from the library!"
+}
+
